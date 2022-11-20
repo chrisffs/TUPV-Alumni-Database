06c086e38956b5fce998f06825c875588e1056f3
Christian Noel Salvador <christiannoelsalvador24@gmail.com>
2022-11-20 23:33:35 +0800

    auqna

--- index.php
+++ index.php
@@ -4,17 +4,88 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- FONTS -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
     <title>Document</title>
     <link rel="stylesheet" href="css/style.css" type="text/css">
-
     <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
 </head>
-<body>
-    
+<body style="background-color: #FAFAFA;">
+    <section id="home">
+        <!-- NAVBAR -->
+        <nav class="navbar navbar-expand-md navbar-light pt-md-3">
+            <div class="container">
+                <a href="#home" class="navbar-brand d-flex align-items-center " id="logo">
+                        <img src="img/tup-logo.png" alt="TUPV Logo" id="logo">
+                        <div class="collapse navbar-collapse">
+                            <p class="logo-text fw-pp mb-0 ms-2">
+                            Technological University of the Philippines Visayas
+                            </p>
+                        </div>
+                </a>
+
+                <button 
+                class="navbar-toggler" 
+                type="button" 
+                data-bs-toggle="collapse"
+                data-bs-target="#nav-menu"
+                >
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="nav-menu">
+                    <ul class="navbar-nav ms-auto d-flex align-items-center" id="nav-link-ul">
+                        <li class="nav-item mx-1 ">
+                            <a href="" class="nav-link fw-pp">Home</a>
+                        </li>
+                        <li class="nav-item mx-1">
+                            <a href="" class="nav-link fw-pp">Alumni</a>
+                        </li>
+                        <li class="nav-item mx-1">
+                            <a href="" class="nav-link fw-pp">Gallery</a>
+                        </li>
+                        <li class="nav-item mx-1">
+                            <a href="" class="nav-link fw-pp">About</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- NAVBAR END -->
+
+        <!-- HERO -->
+        <section class="bg-light py-5">
+            <div class="container">
+                <div class="d-flex flex-lg-row flex-column-reverse justify-content-around align-items-center">
+                    <div class="text-lg-start" id="title-l">
+                        <p class="blck-txt fw-pp">Welcome to <span class="span1">TUPV</span></p>
+                        <p class="hl-txt fw-pp">Alumni Website</p>
+                        <p class="desc fw-pp">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                    </div>
+                    <img class="img-fluid img-r" src="img/Group 1.png" alt="" srcset="">
+                </div>
+                
+            </div>
+        </section>
+        <!-- HERO END-->
+        <section class="py-5">
+            <div class="container bg-primary1" id="">
+awdawdad
+            </div>
+        </section>
+    </section>
+
+
     <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
